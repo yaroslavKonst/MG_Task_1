28f03d2fed3266ba28ec93c2a7c7f33aa2b712d0
yaroslavKonst <yaroslav.konst@yandex.ru>
2020-03-19 22:12:08 +0300

    Coordinate system fixed

--- main.cpp
+++ main.cpp
@@ -51,31 +51,31 @@ int main(int argc, const char **argv)
 
 	std::cout << "Loading models.\n";
 
-	Sphere sp_bl(Vertex3(-20, 0, 300), 50, Vector3(0, 0, 0.9),
+	Sphere sp_bl(Vertex3(0, -20, 300), 50, Vector3(0, 0, 0.9),
 		Vector3(1, 1, 1), 300, 1, 140);
 
-	Sphere sp_bl1(Vertex3(-40, 80, 350), 20, Vector3(0, 0.9, 0),
+	Sphere sp_bl1(Vertex3(-80, -40, 350), 20, Vector3(0, 0.9, 0),
 		Vector3(1, 1, 1), 500, 1, 140);
 
-	Mesh plane("objects/planeAssembly.obj", 140, 100, 400, 3*M_PI/4, -M_PI/4,
-		-M_PI / 2);
+	Mesh plane("objects/planeAssembly.obj", -100, 140, 400, M_PI, M_PI / 8,
+		-M_PI / 10);
 
-	Sphere sp(Vertex3(100, -100, 700), 50, Vector3(0.7, 0.7, 0.7),
+	Sphere sp(Vertex3(100, 100, 700), 50, Vector3(0.7, 0.7, 0.7),
 		Vector3(1, 1, 1), 800, 1, 140);
 
-	Sphere sp1(Vertex3(200, 0, 800), 30, Vector3(0.8, 0, 0),
+	Sphere sp1(Vertex3(0, 50, 800), 30, Vector3(0.8, 0, 0),
 		Vector3(1, 1, 1), 200, 1, 140);
 
-	Sphere sp_gr(Vertex3(200, -150, 600), 50, Vector3(0, 0.9, 0),
+	Sphere sp_gr(Vertex3(150, 200, 600), 50, Vector3(0, 0.9, 0),
 		Vector3(1, 1, 1), 300, 1, 140);
 
 	Endless_plane plain;
-	plain.vertices[0] = Vertex3(-50, -500, 500);
-	plain.vertices[1] = Vertex3(-50, 500, 500);
-	plain.vertices[2] = Vertex3(-50, 0, 0);
-	plain.normals[0] = Vector3(1, 0, 0);
-	plain.normals[1] = Vector3(1, 0, 0);
-	plain.normals[2] = Vector3(1, 0, 0);
+	plain.vertices[0] = Vertex3(-500, -50, 500);
+	plain.vertices[1] = Vertex3(500, -50, 500);
+	plain.vertices[2] = Vertex3(0, -50, 0);
+	plain.normals[0] = Vector3(0, 1, 0);
+	plain.normals[1] = Vector3(0, 1, 0);
+	plain.normals[2] = Vector3(0, 1, 0);
 	plain.mat.Kd = Vector3();
 	plain.mat.Ks = Vector3();
 	plain.mat.Ns = 100;
@@ -85,11 +85,11 @@ int main(int argc, const char **argv)
 	plain.textures[1] = Vector3(500, 500, 0);
 	plain.textures[2] = Vector3(0, 0, 0);
 
-	lights.push_back(Light(Vertex3(100, 500, 0), 500000));
-	lights.push_back(Light(Vertex3(100, -500, 200), 100000));
-	lights.push_back(Light(Vertex3(0, -400, 400), 20000));
-	lights.push_back(Light(Vertex3(100, 200, 900), 100000));
-	lights.push_back(Light(Vertex3(100, -400, 900), 100000));
+	lights.push_back(Light(Vertex3(-500, 100, 0), 500000));
+	lights.push_back(Light(Vertex3(500, 100, 200), 100000));
+	lights.push_back(Light(Vertex3(400, 0, 400), 20000));
+	lights.push_back(Light(Vertex3(-200, 100, 900), 100000));
+	lights.push_back(Light(Vertex3(400, 100, 900), 100000));
 
 	objects.push_back(&sp_bl);
 	objects.push_back(&sp_bl1);
@@ -100,13 +100,13 @@ int main(int argc, const char **argv)
 	objects.push_back(&plain);
 
 	Camera cam;
-	cam.position = Vertex3(100, 0, 0);
-	cam.phi = -M_PI / 16;
-	cam.psi = 0;
+	cam.position = Vertex3(0, 100, 0);
+	cam.phi = 0;
+	cam.psi = M_PI / 16;
 	cam.fov = M_PI / 2;
 
-	uint32_t width = 20000;
-	uint32_t height = 20000;
+	uint32_t width = 2000;
+	uint32_t height = 2000;
 
 	std::cout << "Rendering.\n";
 
@@ -143,7 +143,7 @@ int main(int argc, const char **argv)
 
 			#pragma omp critical
 			{
-				image[i*height + j] = color;
+				image[j*height + width - i] = color;
 			}
 		}
 	}
